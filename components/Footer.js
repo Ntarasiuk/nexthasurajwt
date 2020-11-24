ec728f863cf8595da916f0ee481e10e43b7d7046
Nathan Tarasiuk <ntarasiuk@gmail.com>
2020-11-24 08:19:23 -0800

    refactored footer

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://www.linkedin.com/in/ntarasiuk"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by Nate Tarasiuk
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
